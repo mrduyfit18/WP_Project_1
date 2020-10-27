fefd0169937ba72c00119f28723aa6ff741f99de
mrduyfit18 <56330329+mrduyfit18@users.noreply.github.com>
2020-10-27 18:29:36 +0700

    add Class

---
+++ FoodRecipe/AddRecipeWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+
+namespace FoodRecipe
+{
+    /// <summary>
+    /// Interaction logic for AddRecipeWindow.xaml
+    /// </summary>
+    public partial class AddRecipeWindow : Window
+    {
+        class Step : INotifyPropertyChanged
+        {
+            public int Order { get; set; }              //thứ tự bước
+            public string ImagePath { get; set; }       //đường dẫn ảnh của bước
+            public string Content { get; set; }         //mô tả bước
+
+            public event PropertyChangedEventHandler PropertyChanged;
+        }
+
+        
+
+        public AddRecipeWindow()
+        {
+            InitializeComponent();
+        }
+
+   
+        private void newClick(object sender, RoutedEventArgs e)
+        {
+            var a = new ListBoxItem();
+            a.Template = newlist.Resources["addStepForm"] as ControlTemplate;
+            newlist.Children.Add(a);
+        }
+    }
+
+}
--- FoodRecipe/HomeScreen.xaml.cs
+++ FoodRecipe/HomeScreen.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.ComponentModel;
 
 namespace FoodRecipe
 {
@@ -19,6 +23,20 @@ namespace FoodRecipe
     /// </summary>
     public partial class HomeScreen : Window
     {
+
+        class Food : INotifyPropertyChanged
+        {
+            public string ID { get; set; }              //ID món ăn 
+            public string Name { get; set; }            // Tên món ăn
+            public string Material { get; set; }        //Danh sách nguyên liệu
+            public bool IsFavorite { get; set; }        //Món yêu thích
+            public DateTime DateAdd { get; set; }       //ngày thêm
+            public string ImagePath { get; set; }       //đường dẫn ảnh
+            public string VideoLink { get; set; }       //link video
+
+            public event PropertyChangedEventHandler PropertyChanged;
+        }
+
         public HomeScreen()
         {
             InitializeComponent();
